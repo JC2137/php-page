d7132b3fd596eae9d6b81c0ce12909eb88bb396c
JC2137 <156438197+JC2137@users.noreply.github.com>
2024-04-19 15:08:20 +0200

    Create busc.php

---
+++ busc.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Buscador PHP</title>
+</head>
+<body>
+    <h2>Buscador de Productos</h2>
+    <form method="GET" action="buscador.php">
+        <input type="text" name="Nombre" placeholder="Ingrese su búsqueda">
+        <input type="submit" value="Buscar">
+    </form>
+</body>
+</html>
