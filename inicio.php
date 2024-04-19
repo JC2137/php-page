eae9c1634a88454aab81b0c178521dfed88ec9f9
JC2137 <156438197+JC2137@users.noreply.github.com>
2024-04-19 15:07:17 +0200

    Create inicio.php

---
+++ inicio.php
@@ -0,0 +1,6 @@
+<FORM ACTION="login.php" METHOD="POST">
+Usuario:<INPUT TYPE="text" NAME="User"><br/>
+Contraseña:<INPUT TYPE="text" NAME="Contraseña"><br/>
+<INPUT TYPE="submit" NAME="proc">
+
+</FORM>
