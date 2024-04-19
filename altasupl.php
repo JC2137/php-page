21e64aae4860da7cf357a9f81698fac5ce6b260b
JC2137 <156438197+JC2137@users.noreply.github.com>
2024-04-19 15:06:53 +0200

    Create altasupl.php

---
+++ altasupl.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <title>
+            conexion con la base de datos
+        </title>
+    </head>
+    <body>
+        <FORM ACTION="c.php" METHOD="POST">
+        Nombre:<INPUT TYPE="text" NAME="Nombre"><br/>
+        Apellidos:<INPUT TYPE="text" NAME="Apellidos"><br/>
+        Usuario:<INPUT TYPE="text" NAME="User"><br/>
+        Contraseña:<INPUT TYPE="text" NAME="Contraseña"><br/>
+        Email:<INPUT TYPE="text" NAME="Email"><br/>
+        <INPUT TYPE="submit" NAME="proc">
+
+</FORM>
+</body>
+</html>
